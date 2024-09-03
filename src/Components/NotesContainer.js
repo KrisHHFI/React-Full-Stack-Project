d44261ac57b6dad779ffd71a985195a24c675297
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-03 22:50:03 +0300

    No notes found text added

--- src/Components/NotesContainer.js
+++ src/Components/NotesContainer.js
@@ -3,19 +3,21 @@ import Note from './Note';
 
 function NotesContainer({ notes = [], deleteNote }) {
 
-  //console.log("NotesContainer notes:" + JSON.stringify(notes, null, 2));
-
   return (
     <div className="noteContainer">
-      {notes.map((note) => (
-        <Note
-          deleteNote={deleteNote}
-          key={note.id}
-          noteTitle={note.noteTitle}
-          noteText={note.noteText}
-          id={note.id}
-        />
-      ))}
+      {notes.length === 0 ? (
+        <p>No notes found.</p>
+      ) : (
+        notes.map((note) => (
+          <Note
+            deleteNote={deleteNote}
+            key={note.id}
+            noteTitle={note.noteTitle}
+            noteText={note.noteText}
+            id={note.id}
+          />
+        ))
+      )}
     </div>
   );
 }
