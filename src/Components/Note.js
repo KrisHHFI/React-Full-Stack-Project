0355dea62156675243224fa0b000430d7b06d5d1
KrisHHFI <kristopherpepperuk@gmail.com>
2024-09-03 08:55:19 +0300

    Note text inputs added

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Note({ noteTitle, noteText,}) {
+function Note({ noteTitle, noteText }) {
+    const [title, setTitle] = useState(noteTitle);
+    const [text, setText] = useState(noteText);
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleTextChange = (e) => {
+        setText(e.target.value);
+    };
+
     return (
         <div className="note">
-            <div className="noteTitle">
-                {noteTitle}
-            </div>
-            <div className="noteText">
-                {noteText}
-            </div>
+            <input
+                type="text"
+                value={title}
+                onChange={handleTitleChange}
+            />
+            <textarea
+                className="noteText"
+                value={text}
+                onChange={handleTextChange}
+            />
         </div>
     );
 }
